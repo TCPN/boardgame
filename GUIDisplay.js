ba8a9441e2c6c3896f33ce07fa15e64e3968ce0e
TCPN <boyenen@gmail.com>
2016-10-09 01:03:53 +0800

    message box function fixing * move 'displayMessage' out of the 'GUIDisplay' * change delay timeout when first time create message box to 100 + add a greeting message, to create message box earlier

--- GUIDisplay.js
+++ GUIDisplay.js
@@ -13,28 +13,6 @@ function GUIDisplay(game, screen, handlers)
 				;
 		}
 	};
-	function displayMessage(message, kind, keepTime){
-		var msgDOM = screen.getElementsByClassName('message')[0];
-		function hideMessageDOM(){msgDOM.classList.add('hidden')};
-		function showMessageDOM(){msgDOM.classList.remove('hidden')};
-		if(msgDOM == undefined)
-		{
-			msgDOM = screen.insertBefore(document.createElement('div'),null);
-			msgDOM.classList.add('message','hidden');
-			/*
-			msgDOM.addEventListener('transitioned', function(){
-				if(!msgDOM.classList.contains('hidden') && !msgDOM.classList.contains('noAutoHide'))
-					setTimeout(hideMessageDOM, msgDOM.keepTime);
-			}, true);*/
-			msgDOM.addEventListener('click', hideMessageDOM);
-			setTimeout(function(){displayMessage(message, kind, keepTime)}, 1);// run like this, so the message animation will played.
-			return;
-		}
-		msgDOM.innerHTML = message;
-		msgDOM.keepTime = keepTime * 1000;
-		showMessageDOM();
-		setTimeout(hideMessageDOM, keepTime * 1000);
-	};
 	function updateOptable(obj, objDOM)
 	{
 		if(obj.optionIndex > -1)
@@ -151,3 +129,27 @@ function GUIDisplay(game, screen, handlers)
 	}
 }
 
+function displayMessage(message, kind, keepTime){
+	var msgDOM = document.getElementsByClassName('message')[0];
+	function hideMessageDOM(){msgDOM.classList.add('hidden')};
+	function showMessageDOM(){msgDOM.classList.remove('hidden')};
+	if(msgDOM == undefined)
+	{
+		msgDOM = document.createElement('div');
+		msgDOM.classList.add('message','hidden');
+		msgDOM = document.getElementById('mainFrame').insertBefore(msgDOM,null);
+		/*
+		msgDOM.addEventListener('transitioned', function(){
+			if(!msgDOM.classList.contains('hidden') && !msgDOM.classList.contains('noAutoHide'))
+				setTimeout(hideMessageDOM, msgDOM.keepTime);
+		}, true);*/
+		msgDOM.addEventListener('click', hideMessageDOM);
+		setTimeout(function(){displayMessage(message, kind, keepTime)}, 100);// run like this, so the message animation will played.
+		return;
+	}
+	msgDOM.innerHTML = message;
+	msgDOM.keepTime = keepTime * 1000;
+	showMessageDOM();
+	setTimeout(hideMessageDOM, keepTime * 1000);
+};
+displayMessage("Welcome!", 'greetings', 2);
